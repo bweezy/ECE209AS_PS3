5d23c7db7fb4aa398a4307ab76b1cf4bc45baa71
bweezy <bwood94@ucla.edu>
2018-10-31 15:13:07 -0700

    added test routine

--- twr_test.py
+++ twr_test.py
@@ -5,7 +5,7 @@ import numpy as np
 
 robot = twr.TwoWheeledRobot(initial_state=state.State(250,375, np.pi / 2.0))
 
-
+'''
 print 'initial_state '
 print robot.real_state.get_state_degrees()
 print
@@ -30,3 +30,29 @@ print 'estimated'
 print robot.estimated_state_mean.get_state_degrees()
 print robot.covariance
 print
+'''
+
+while(True):
+	w_l = input("Input w_l: ")
+	w_r = input("Input w_r: ")
+
+	robot.time_update(Input.Input(w_l, w_r))
+
+	real_state = np.array([robot.real_state.get_state_degrees()])
+	estimated_state_mean = np.array([robot.estimated_state_mean.get_state_degrees()])
+	error = real_state - estimated_state_mean
+
+	print '--movement update--'
+	print 'error: ',
+	print error
+
+	robot.measurement_update()
+	real_state = np.array([robot.real_state.get_state_degrees()])
+	estimated_state_mean = np.array([robot.estimated_state_mean.get_state_degrees()])
+	error = real_state - estimated_state_mean
+	print '--measurement_update--'
+	print 'error: ',
+	print error
+
+
+
