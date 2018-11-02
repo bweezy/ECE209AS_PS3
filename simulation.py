88e45da5e009827416068218772df12b6c2021bb
bweezy <bwood94@ucla.edu>
2018-11-02 16:02:09 -0700

    commented

--- simulation.py
+++ simulation.py
@@ -8,59 +8,59 @@ import matplotlib.patches as patches
 
 np.random.seed(1)
 
+# Define the environment bounds
 env_W = 500
 env_L = 750
                 
 def run_simulation(init_state):
 
+    # Create our robot class
     robot = twr.TwoWheeledRobot(initial_state=init_state)
+    # The constant input given
     u = [1, 2]
 
     fig, ax = plt.subplots(1)
     
     trajectory = []
-    trajectory.append(robot.real_state.get_state_degrees())
+    # Append the initial state estimate and plot
     estimated_state_mean = robot.estimated_state_mean.get_state_degrees()
     covariance = robot.covariance
     plot_covariance_ellipse((estimated_state_mean[0], estimated_state_mean[1]), 
             covariance[0:2, 0:2], facecolor='k', alpha=0.3)
     for i in range(15):
-        #robot.time_update(Input.Input(u[0], u[1]))
 
+        # Find the real state after each step
         real_state = robot.real_state.get_state_degrees()
         trajectory.append(real_state)
 
+        # Plot the estimate and covariance matrix after each time update
+        robot.time_update(Input.Input(u[0], u[1]))
         estimated_state_mean = robot.estimated_state_mean.get_state_degrees()
         covariance = robot.covariance
-
-        # # import pdb; pdb.set_trace()
-        # # print i, " iteration: "
-        # # print "  ", estimated_state_mean[0], estimated_state_mean[1]
-        print i
-        print covariance
         plot_covariance_ellipse((estimated_state_mean[0], estimated_state_mean[1]), 
             covariance[0:2, 0:2], facecolor='k', alpha=0.3)
-        robot.time_update(Input.Input(u[0], u[1]))
+
+        # Plot the estimate and covariance matrix after each measurement update
         robot.measurement_update()
         estimated_state_mean = robot.estimated_state_mean.get_state_degrees()
         covariance = robot.covariance
-
-        # print "  ", estimated_state_mean[0], estimated_state_mean[1]
         plot_covariance_ellipse((estimated_state_mean[0], estimated_state_mean[1]), 
             covariance[0:2, 0:2], facecolor='g', alpha=0.8)
 
+
+    # Plot the real trajectory
     trajectory.append(robot.real_state.get_state_degrees())
     trajectory = np.array(trajectory)
     plt.plot(trajectory[:, 0], trajectory[:,1], color='k', lw=2)
+
+    # Plot the room bounds
     rect = patches.Rectangle((0,0),env_W,env_L,linewidth=1,edgecolor='r',facecolor='none')
     plt.ylim(-10, env_L+10)
     plt.xlim(-10, env_W+10)
     ax.add_patch(rect)
-    # if ylim is not None: plt.ylim(*ylim)
     plt.show()
 
 
 if __name__ == "__main__":
     initial_state = state.State(400,375, np.pi / 2.0)
-    # initial_state = state.State(100,100, np.pi / 2.0)
     run_simulation(initial_state)
--- twowheeledrobot.py
+++ twowheeledrobot.py
@@ -7,21 +7,24 @@ class TwoWheeledRobot:
 	def __init__(self, b=85.0, r=20.0, initial_state=state.State(0,0,0), f_s=1):
 		self.b = b # Distance between wheels
 		self.r = r # Radius of wheels
-		self.real_state = initial_state
-		self.estimated_state_mean = initial_state
-		self.covariance = np.zeros((3,3)) 
-		self.d_t = 1.0/f_s
+		self.real_state = initial_state # Recording of real state
+		self.estimated_state_mean = initial_state # Recording of estimated state mean
+		self.covariance = np.zeros((3,3))  #Initial covariance matrix, set to zero for known initial state
+		self.d_t = 1.0/f_s # Time step
+		self.Q = np.eye(2) * (np.pi / 6.0) ** 2 # Process noise variance
+		self.R = np.eye(2) * (9.375) ** 2 # Measurement noise variance
 
-		self.Q = np.eye(2) * (np.pi / 6.0) ** 2 
-		self.R = np.eye(2) * (9.375) ** 2
 
-
+	# Movement 
 	def time_update(self, u):
 
+		# Update the real state - includes noise
 		self.real_state_update(u)
+		# Update the state estimate - does not include noise
 		self.estimated_state_update(u)
 
 
+	# Updating the real state
 	def real_state_update(self, u):
 
 		w_l, w_r = u.get_input()
@@ -30,25 +33,29 @@ class TwoWheeledRobot:
 		r = self.r
 		d_t = self.d_t
 
-		### Real State Update ###
+		# Add noise to the input
 		w_l, w_r = u.get_input()
 		w_l = w_l + np.pi / 6.0 * np.random.randn() 
 		w_r = w_r + np.pi / 6.0 * np.random.randn()
 
 		x, y, theta = self.real_state.get_state()
+
+		# Update theta
 		theta_new = (d_t * r / b * (w_r - w_l) + theta) % (2 * np.pi) 
 
+		# Update x and y given theta and theta new
 		if w_r == w_l:
 			x = x + r * w_r * d_t * np.cos(theta)
 			y = y + r * w_r * d_t * np.sin(theta)
 		else:
-			R = b / 2.0 * (w_r + w_l)/(w_r - w_l)
+			R = b / 2.0 * (w_r + w_l)/(w_r - w_l) # Circle radius
 			x = x + R * (np.sin(theta_new) - np.sin(theta))
 			y = y - R * (np.cos(theta_new) - np.cos(theta))
 
 		self.real_state = state.State(x, y, theta_new)
 
 
+	# Updating the state estimate
 	def estimated_state_update(self, u):
 
 		w_l, w_r = u.get_input()
@@ -58,8 +65,10 @@ class TwoWheeledRobot:
 		d_t = self.d_t
 
 		x, y, theta = self.estimated_state_mean.get_state()
+		# Update theta using non-noisy input
 		theta_new = (d_t * r / b * (w_r - w_l) + theta) % (2 * np.pi) 
 
+		#Update x and y given theta and theta new
 		if w_r == w_l:
 			x = x + r * w_r * d_t * np.cos(theta)
 			y = y + r * w_r * d_t * np.sin(theta)
@@ -69,17 +78,21 @@ class TwoWheeledRobot:
 			R = b / 2.0 * (w_r + w_l)/(w_r - w_l)
 			x = x + R * (np.sin(theta_new) - np.sin(theta))
 			y = y - R * (np.cos(theta_new) - np.cos(theta))
+			# Derivative of state updates with respect to theta
 			df0_dtheta = R * (np.cos(theta_new) - np.cos(theta))
 			df1_dtheta = R * ( -1.0 * np.sin(theta_new) + np.sin(theta))
 
+		# Create dynamics jacobian
 		F = np.eye(3)
 		F[0][2] = df0_dtheta
 		F[1][2] = df1_dtheta
 		W = self.get_process_noise_jacobian(u)
 		self.estimated_state_mean = state.State(x, y, theta_new)
+		# Update covariance with dynamics and process noise jacobians
 		self.covariance = F.dot(self.covariance).dot(F.T) + W.dot(self.Q).dot(W.T)
 
 
+	# Calculates the process noise jacobian given the input
 	def get_process_noise_jacobian(self, u):
 
 		b = self.b
@@ -87,6 +100,7 @@ class TwoWheeledRobot:
 		w_l, w_r = u.get_input()
 		x, y, theta = self.estimated_state_mean.get_state()
 
+		# See report for derivations
 		if w_r == w_l:
 			dx_dwl = r * w_l / (2.0 * b) * np.sin(theta)
 			dx_dwr = -1.0 * r * w_r / (2.0 * b) * np.sin(theta)
@@ -110,46 +124,54 @@ class TwoWheeledRobot:
 		return W
 		
 
-
-	# This is super Brute Force, should be a better way
+	# Update state and covariance given measurements
 	def measurement_update(self):
 
 		x, y, theta = self.real_state.get_state()
 
+		# Calculate rangefinder results given the state
 		y_t, front_wall_idx, right_wall_idx = self.measure(self.real_state)
 		# Right here we are currently returning which walls the sensors are measuring
 		# This helps us use the correct measurement model to compute the Jacobian
 		# I think this is technically cheating -> should maybe compute all jacobians and choose one with lowest estimation error?
 		H = self.get_observation_jacobian(x, y, theta, front_wall_idx, right_wall_idx)
 
+		# Add noise to the measurement
 		y_t = (np.array([y_t.get_measurement()]) + np.random.randn() * (9.375)).T
+
+		# Create prediction given the estimated state
 		predicted_y_t, _, _ = self.measure(self.estimated_state_mean)
 		predicted_y_t = np.array([predicted_y_t.get_measurement()]).T
 
 	
 		sigma_m = self.covariance
 		inv_mat = np.linalg.inv(H.dot(sigma_m).dot(H.T) + self.R)
-
 		x_hat = np.array([self.estimated_state_mean.get_state()]).T
 
-		#print sigma_m.shape
-		#print H.T.shape
-		#print inv_mat.shape
-		#print y_t.shape
-		#print x_hat.shape
-		# Update Step
+		# Update estimate and covariance given observation jacobian
 		x_hat += sigma_m.dot(H.T).dot(inv_mat).dot((y_t - predicted_y_t))
-		self.covariance = sigma_m - sigma_m.dot(H.T).dot(inv_mat).dot(H).dot(sigma_m)
-
 		x_hat = np.squeeze(x_hat)
+		self.covariance = sigma_m - sigma_m.dot(H.T).dot(inv_mat).dot(H).dot(sigma_m)
 		self.estimated_state_mean = state.State(x_hat[0], x_hat[1], x_hat[2])
 
 
-		
+	# Returns the observation jacobian given the state and which wall each sensor is reading
 	def get_observation_jacobian(self, x, y, theta, front_wall_idx, right_wall_idx):
 
-		# Derivative of df with respect to x assuming that f is seeing the front wall
+		# Define walls:
+		#             2 (top)
+		#         ----------------
+		#         |              |
+		#         |              |
+		# 3(left) |              | 1 (right)
+		#         |              |
+		#         |              |
+		#         |              |
+		#         ---------------- 
+		#            4 (bottom)
+
 		# Can save a lot of computation time if we just compute all cos values up front
+		# dd<a><b>_d<c> = partial derivative of rangefinder facing <a> and reading wall <b> with respect to <c>
 		ddf1_dx = -1.0 / np.cos(theta)
 		ddf2_dx = 0
 		ddf3_dx = 1.0 / np.cos(theta - np.pi)
@@ -195,6 +217,7 @@ class TwoWheeledRobot:
 
 
 
+	# Given a state, return the rangefinder results
 	def measure(self, state_in):
 
 		# Define walls:
@@ -211,6 +234,7 @@ class TwoWheeledRobot:
 
 		x, y, theta = state_in.get_state()
 
+		# Calculate the right rangefinder results for each wall
 		dr1 = (500 - x) / np.cos(theta - np.pi / 2.0)
 		dr2 = (750 - y) / np.cos(theta - np.pi)
 		dr3 = x / np.cos(theta - np.pi * 3.0 / 2.0)
@@ -220,11 +244,13 @@ class TwoWheeledRobot:
 		min_pos_dr = float("inf")
 		right_wall_idx = None
 
+		# The wall the right rangefinder should read is the smallest positive number of the above values
 		for i in np.arange(len(dr)):
 			if dr[i] > 0 and dr[i] < min_pos_dr:
 				min_pos_dr = dr[i]
 				right_wall_idx = i + 1
 
+		# Calculate the right rangefinder results for each wall
 		df1 = (500 - x) / np.cos(theta)
 		df2 = (750 - y) / np.cos(theta - np.pi / 2.0)
 		df3 = (x) / np.cos(theta - np.pi)
@@ -233,22 +259,10 @@ class TwoWheeledRobot:
 		df = [df1, df2, df3, df4]
 		min_pos_df = float("inf")
 		front_wall_idx = None
-
+		# The wall the front rangefinder should read is the smallest positive number of the above values
 		for i in np.arange(len(df)):
 			if df[i] > 0 and df[i] < min_pos_df:
 				min_pos_df = df[i]
 				front_wall_idx = i + 1
-
-		'''
-		print 'df ',
-		print min_pos_df
-		print 'front wall idx ',
-		print front_wall_idx
-
-		print 'dr ',
-		print min_pos_dr
-		print 'right wall idx ',
-		print right_wall_idx
-		'''
 
 		return measurement.Measurement(min_pos_df, min_pos_dr), front_wall_idx, right_wall_idx
